6ee368a110fe80575a4447e0cb3bb0530bae7f76
Sergej Kukshaus <sergej@kukshaus.com>
2021-08-24 21:25:00 +0200

    project structure created

---
+++ Bankomat.Ui/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bankomat.Ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
